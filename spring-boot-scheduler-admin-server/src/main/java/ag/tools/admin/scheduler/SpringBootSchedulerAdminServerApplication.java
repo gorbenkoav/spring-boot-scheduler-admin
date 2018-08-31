b5b0f54be9b22e1098aa2b5f40ceb56e1332bf51
Lonely Ranger <gorbenkoav@gmail.com>
2018-08-31 15:52:01 +0300

    Initial project structure

---
+++ spring-boot-scheduler-admin-server/src/main/java/ag/tools/admin/scheduler/SpringBootSchedulerAdminServerApplication.java
@@ -0,0 +1,12 @@
+package ag.tools.admin.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchedulerAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSchedulerAdminServerApplication.class, args);
+	}
+}
---
+++ spring-boot-scheduler-admin-starter-client/src/main/java/ag/tools/admin/scheduler/client/SpringBootSchedulerAdminClientApplication.java
@@ -0,0 +1,12 @@
+package ag.tools.admin.scheduler.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchedulerAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSchedulerAdminClientApplication.class, args);
+	}
+}
