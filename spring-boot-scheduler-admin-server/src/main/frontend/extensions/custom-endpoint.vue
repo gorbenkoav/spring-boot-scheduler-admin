a17f7b9ed449faff754df0cac3174b4e83c0e2d7
Lonely Ranger <gorbenkoav@gmail.com>
2018-09-06 10:01:27 +0300

    Add custom endpoint view from spring boot admin custom ui example. It shows just simple info about health. Sample here https://github.com/codecentric/spring-boot-admin/tree/master/spring-boot-admin-samples/spring-boot-admin-sample-custom-ui

---
+++ spring-boot-scheduler-admin-server/src/main/frontend/extensions/custom-endpoint.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="custom">
+        Instance: <span v-text="instance.id"/>
+        Output: <span v-text="text"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            instance: {
+                type: Object,
+                required: true
+            }
+        },
+        data: () => ({
+            text: ''
+        }),
+        async created() {
+            const response = await this.instance.axios.get('actuator/health');
+            this.text = response.data;
+        }
+    };
+</script>
+
+<style>
+    .custom {
+        font-size: 20px;
+        width: 80%;
+    }
+</style>
--- spring-boot-scheduler-admin-server/src/main/java/ag/tools/admin/scheduler/SpringBootSchedulerAdminServerApplication.java
+++ spring-boot-scheduler-admin-server/src/main/java/ag/tools/admin/scheduler/SpringBootSchedulerAdminServerApplication.java
@@ -1,8 +1,10 @@
 package ag.tools.admin.scheduler;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class SpringBootSchedulerAdminServerApplication {
 
